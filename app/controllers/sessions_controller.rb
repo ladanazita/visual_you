f528384c7e1c9e773d5e8d91e370d234fb1540de
Ladan Nasserian <ladannasserian@gmail.com>
2015-04-22 18:58:50 -0700

    created logout method, modified routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+
   def create
+  user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to('/about')
+    else
+      redirect_to('/login')
+    end
   end
+
+  def destroy
+    session[:user_id]=nil
+    redirect_to('/')
+  end
+
 end
