b01c92dfeb7f94bacd7c13cbcd8a8a9f213814ee
Ladan Nasserian <ladannasserian@gmail.com>
2015-04-24 12:57:41 -0700

    generates random image on about page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,10 @@ class WelcomeController < ApplicationController
   def index
   end
   def about
+      @posts = Post.all
+      @min = Post.first.id
+      @max = Post.last.id
+      @randomid = rand(@min...@max)
+      @randompic = Post.find_by_id(@randomid).picture.url
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :user
   mount_uploader :picture, PictureUploader
+
 end
